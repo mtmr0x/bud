f9b41bc799912702436842d0afc5556d99a9066f
Matheus Marsiglio <github@mtmr0x.com>
2025-06-22 19:59:06 -0300

    chore: use build.clj to build pom and jar

---
+++ build.clj
@@ -0,0 +1,24 @@
+(ns build
+  (:require
+    [clojure.tools.build.api :as b]))
+
+(def lib 'org.clojars.mat/bud)
+(def version "0.1.4")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+
+(defn build-pom [_]
+  (b/write-pom {:basis     basis
+                :lib       lib
+                :version   version
+                :target    ""}))
+
+(defn jar [_]
+  (b/copy-dir {:src-dirs   ["src"]
+               :target-dir class-dir})
+  (b/write-pom {:basis     basis
+                :lib       lib
+                :version   version
+                :target    ""})
+  (b/jar {:class-dir class-dir
+          :jar-file  "target/bud.jar"}))
